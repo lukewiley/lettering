89603870f65a7670ada5b891f321f6ad1a998061
Luke <law@eagles.ewu.edu>
2015-04-21 17:36:58 -0700

    Version 2

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,30 @@
 // My Scripts
 
-document.write("Hello World!");
+
+
+
+// Get name and store name
+
+var name = prompt('What is your name')
+
+
+// write to name
+
+$('.name').text(name);
+
+
+
+
+
+
+
+// get major and store major
+var major = prompt('What is your major')
+
+// write to major
+$('.major').text(major);
+
+
+//lettering.js to h1
+
+$('h1').lettering();
